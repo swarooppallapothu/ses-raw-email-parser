d67a50238c7493b66933449eb944759b2d2d8d38
swarooppallapothu <swaroop.pallapothu@gmail.com>
2019-03-28 01:15:56 +0530

    Storing Request object to s3

--- src/main/java/com/aws/lambda/parser/SESRawEmailParser.java
+++ src/main/java/com/aws/lambda/parser/SESRawEmailParser.java
@@ -5,13 +5,14 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.*;
+import com.amazonaws.util.IOUtils;
 import com.amazonaws.util.StringUtils;
 import com.aws.lambda.parser.util.MailParserUtil;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.mail.util.MimeMessageParser;
+import org.apache.http.entity.ContentType;
 
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
@@ -22,29 +23,46 @@ import java.util.stream.Collectors;
 public class SESRawEmailParser extends MailParserUtil implements RequestStreamHandler {
 
     private final static ObjectMapper objectMapper;
+    private final static AmazonS3 S3_CLIENT;
+    private final static String SRC_BKT;
+    private final static String TGT_BKT;
 
     static {
         objectMapper = new ObjectMapper(); // can reuse, share globally
         objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        S3_CLIENT = AmazonS3ClientBuilder.defaultClient();
+        SRC_BKT = "lucroview-net-email-repo";
+        TGT_BKT = "lucroview-net-store";
     }
 
     @Override
     public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
         LambdaLogger logger = context.getLogger();
+        long genMs = System.currentTimeMillis();
+        byte[] requestByteArr = null;
         try {
             if (inputStream != null) {
+                requestByteArr = IOUtils.toByteArray(inputStream);
 
-                String jsonString = isToString(inputStream);
+                logger.log("genMs: " + genMs);
+//                logger.log("IOUtils.toByteArray(is).length: " + IOUtils.toByteArray(is).length);
+                String jsonString = isToString(new ByteArrayInputStream(requestByteArr));
+//                logger.log("IOUtils.toByteArray(is).length: " + IOUtils.toByteArray(is).length);
                 logger.log(jsonString);
                 SESEvent event = objectMapper.readValue(jsonString, SESEvent.class);
                 String messageId = event.getRecords().get(0).getSES().getMail().getMessageId();
+
+                if (!StringUtils.isNullOrEmpty(messageId)) {
+                    storeRequestStream(new ByteArrayInputStream(requestByteArr), messageId + ".json", context);
+                } else {
+                    throw new Exception("No Message Id genMs: " + genMs);
+                }
+
                 logger.log(messageId);
 
                 if (!StringUtils.isNullOrEmpty(messageId)) {
-                    String srcBucket = "lucroview-net-email-repo";
-                    AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();//new AmazonS3Client();
-                    S3Object s3Object = s3Client.getObject(new GetObjectRequest(
-                            srcBucket, messageId));
+                    S3Object s3Object = S3_CLIENT.getObject(new GetObjectRequest(
+                            SRC_BKT, messageId));
                     if (s3Object != null) {
                         InputStream objectData = s3Object.getObjectContent();
 
@@ -67,6 +85,9 @@ public class SESRawEmailParser extends MailParserUtil implements RequestStreamHa
             }
         } catch (Exception e) {
             logger.log(e.getMessage());
+            if (requestByteArr != null) {
+                storeRequestStream(new ByteArrayInputStream(requestByteArr), "invalid_stream_" + genMs, context);
+            }
             throw new RuntimeException(e);
         }
     }
@@ -114,5 +135,21 @@ public class SESRawEmailParser extends MailParserUtil implements RequestStreamHa
     public String isToString(InputStream inputStream) {
         return new BufferedReader(new InputStreamReader(inputStream))
                 .lines().collect(Collectors.joining("\n"));
+    }
+
+    public void storeRequestStream(InputStream is, String dstKey, Context context) {
+        LambdaLogger logger = context.getLogger();
+        try {
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentLength(is.available());
+            metadata.setContentType(ContentType.APPLICATION_JSON.getMimeType());
+
+            PutObjectRequest putObjectRequest = new PutObjectRequest(TGT_BKT, dstKey, is, metadata);
+            PutObjectResult putObjectResult = S3_CLIENT.putObject(putObjectRequest);
+
+        } catch (Exception e) {
+            logger.log(String.join("\n", "Exception occurred in method storeRequestStream: ", e.getMessage()));
+            throw new RuntimeException(e);
+        }
     }
 }
